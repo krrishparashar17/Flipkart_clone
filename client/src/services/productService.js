75605476049610cb0a794bf38ead08feecbc2066
krrish parashar <krrishparashar1@gmail.com>
2026-03-28 12:29:15 +0530

    fix frontend product api routes

--- client/src/services/productService.js
+++ client/src/services/productService.js
@@ -1,18 +1,18 @@
 import api from "./api";
 
 export const getAllProducts = async (search = "", category = "") => {
-  const response = await api.get("/products", {
+  const response = await api.get("/api/products", {
     params: { search, category },
   });
   return response.data;
 };
 
 export const getProductById = async (id) => {
-  const response = await api.get(`/products/${id}`);
+  const response = await api.get(`/api/products/${id}`);
   return response.data;
 };
 
 export const getCategories = async () => {
-  const response = await api.get("/products/categories/all");
+  const response = await api.get("/api/products/categories/all");
   return response.data;
 };
